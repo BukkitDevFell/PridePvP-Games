6ca1be697221574b77951becea8684c857c3df83
Chris H <zrenfire@gmail.com>
2012-06-02 20:34:51 -0400

    Some delegators to the data stored in the config. Accessing it like this makes me die a little inside.

--- src/com/pridemc/games/arena/Arena.java
+++ src/com/pridemc/games/arena/Arena.java
@@ -1,6 +1,14 @@
 package com.pridemc.games.arena;
 
+import com.pridemc.games.Core;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.util.Vector;
+
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -40,5 +48,29 @@ public class Arena {
 
 	protected boolean addPlayer(ArenaPlayer arenaPlayer) {
 		return players.add(arenaPlayer);
+	}
+
+	public List<Vector> getGameSpawnVectors() {
+		List<Vector> vectors = new ArrayList<Vector>();
+		List configList = Core.arenas.getList(getName() + ".gamepoints");
+		if (configList != null) {
+			//TODO: Fuck this unsafe casting
+			vectors = (List<Vector>)configList;
+		}
+		return vectors;
+	}
+
+	public World getWorld() {
+		String worldName = Core.arenas.getString(getName() + ".world");
+		return Bukkit.getWorld(worldName);
+	}
+
+	public List<Location> getGameSpawnPoints() {
+		World world = getWorld();
+		List<Location> gameSpawnPoints = new ArrayList<Location>();
+		for (Vector vector : getGameSpawnVectors()) {
+			gameSpawnPoints.add(new Location(world, vector.getX(), vector.getY(), vector.getZ()));
+		}
+		return gameSpawnPoints;
 	}
 }
